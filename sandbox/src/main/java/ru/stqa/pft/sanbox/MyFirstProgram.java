de95dc1ed243c3c74f4c581556e38833bca5d5df
Ivan <imeshcheriyakov@gmail.com>
2018-11-28 22:08:40 +0300

    1.5

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -2,13 +2,11 @@ package ru.stqa.pft.sanbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
     System.out.println(2 + 2);
-    System.out.println(2 - 2);
-    System.out.println(2 / 2);
-    System.out.println(2 * 2);
-    System.out.println(1.0 / 3.0);
-
-    System.out.println("2" + "2");
+    double l = 8;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 }

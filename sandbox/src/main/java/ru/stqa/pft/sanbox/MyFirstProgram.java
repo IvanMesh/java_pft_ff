aaa58d92b63b5df7b0c3981227a04596e568d5b1
Ivan <imeshcheriyakov@gmail.com>
2018-11-29 20:54:36 +0300

    1.7

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -1,12 +1,28 @@
 package ru.stqa.pft.sanbox;
 
+import javax.print.DocFlavor;
+
 public class MyFirstProgram {
   public static void main(String[] args) {
-    String somebody = "world";
+    hello("world");
+
+    double l = 5;
+    System.out.println("Плозадь квадрата со стороной " + l + " = " + area(l));
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+
     System.out.println("Hello, " + somebody + "!");
-    System.out.println(2 + 2);
-    double l = 8;
-    double s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+  }
+
+  public static double area (double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 }

cda50fa58782c1be44b79c89ecbc6be3f025a0a1
Ivan <imeshcheriyakov@gmail.com>
2018-11-29 22:52:03 +0300

    корявый 1 этап домашки

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -6,23 +6,30 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
 
-    double l = 5;
-    System.out.println("Плозадь квадрата со стороной " + l + " = " + area(l));
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Square s = new Square(5);
+    System.out.println("Плозадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4, 6);
+    r.a = 4;
+    r.b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    double x1 = 5;
+    double x2 = 2;
+    double y1 = 7;
+    double y2 = 3;
+    System.out.println("Расстояние между точками с р1 и р2 = " + distance(x1, x2, y1, y2));
   }
 
   public static void hello(String somebody) {
 
     System.out.println("Hello, " + somebody + "!");
   }
+  public static double distance(double x1, double x2, double y1, double y2){
 
-  public static double area (double l) {
-    return l * l;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
+    double p1 = (x1 - x2) * (x1 - x2);
+    double p2 = (y1 - y2) * (y1 - y2);
+    double sqrt = Math.sqrt(p1 + p2);
+    return sqrt;
   }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sanbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double len) {
+    this.l = len;
+  }
+  public double area() {
+    return this.l * this.l;
+  }
+}

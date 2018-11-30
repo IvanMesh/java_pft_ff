00d41006c2f2aaa90d35539e7218a7318a89e59f
Ivan <imeshcheriyakov@gmail.com>
2018-11-30 21:32:50 +0300

    1 полностью

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -14,22 +14,13 @@ public class MyFirstProgram {
     r.b = 6;
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    double x1 = 5;
-    double x2 = 2;
-    double y1 = 7;
-    double y2 = 3;
-    System.out.println("Расстояние между точками с р1 и р2 = " + distance(x1, x2, y1, y2));
+    Point d = new Point(5, 2, 7, 3);
+    System.out.println("Расстояние между точками с р1 и р2 = " + d.distance());
   }
 
   public static void hello(String somebody) {
 
     System.out.println("Hello, " + somebody + "!");
   }
-  public static double distance(double x1, double x2, double y1, double y2){
 
-    double p1 = (x1 - x2) * (x1 - x2);
-    double p2 = (y1 - y2) * (y1 - y2);
-    double sqrt = Math.sqrt(p1 + p2);
-    return sqrt;
-  }
 }
